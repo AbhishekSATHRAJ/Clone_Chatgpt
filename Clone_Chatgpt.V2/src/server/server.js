e8b802a9d7feb332d6b040061bb0c70fa284cafb
AbhishekSATHRAJ <sathrajabhishek78@gmail.com>
2024-12-20 15:19:21 +0530

    Update server.js

--- Clone_Chatgpt.V2/src/server/server.js
+++ Clone_Chatgpt.V2/src/server/server.js
@@ -26,7 +26,7 @@ const app = express();
 
 // Middleware
 app.use(cors(
-  { origin: "http://localhost:5173",
+  { origin: "https://localhost:5173",
    credentials: true,
   }));
 
@@ -36,7 +36,7 @@ app.use(bodyParser.json());
 
 
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "http://localhost:5173"); 
+  res.header("Access-Control-Allow-Origin", "https://localhost:5173"); 
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE"); 
   res.header("Access-Control-Allow-Headers", "Content-Type, Authorization"); 
   res.header("Access-Control-Allow-Credentials", "true"); 
